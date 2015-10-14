ab25233d14665bb701d987454c65b66992ded4f4
Jianan <qinxiandiqi@foxmail.com>
2015-10-14 12:28:01 +0000

    add compile script.

---
+++ Client/compile.bat
@@ -0,0 +1,1 @@
+javac -cp ../Annotations/target/annotationprocessordemo-annotations-1.0-SNAPSHOT.jar;../Processors/target/annotationprocessordemo-processors-1.0-SNAPSHOT.jar src/main/java/com/qinxiandiqi/annotationprocessordemo/client/SimpleAnnotationsTest.java
---
+++ Client/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$JAVA_HOME/bin/javac -cp ../Annotations/target/annotationprocessordemo-annotations-1.0-SNAPSHOT.jar;../Processors/target/annotationprocessordemo-processors-1.0-SNAPSHOT.jar src/main/java/com/qinxiandiqi/annotationprocessordemo/client/SimpleAnnotationsTest.java
