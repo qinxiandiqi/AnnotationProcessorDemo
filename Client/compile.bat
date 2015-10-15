c2ee3195de0b864b85835228d24b9969de51c547
Jianan <qinxiandiqi@foxmail.com>
2015-10-15 10:06:50 +0000

    1.support output the compile log of annotation processor to log file. 2.update compile script. 3.add log file to ignore.

--- Client/compile.bat
+++ Client/compile.bat
@@ -1,1 +1,1 @@
-javac -cp ../Annotations/target/annotationprocessordemo-annotations-1.0-SNAPSHOT.jar;../Processors/target/annotationprocessordemo-processors-1.0-SNAPSHOT.jar src/main/java/com/qinxiandiqi/annotationprocessordemo/client/SimpleAnnotationsTest.java
+javac -cp ../Annotations/target/annotationprocessordemo-annotations-1.0-SNAPSHOT.jar;../Processors/target/annotationprocessordemo-processors-1.0-SNAPSHOT.jar -d target/classes/com/qinxiandiqi/annotationprocessordemo/client/ src/main/java/com/qinxiandiqi/annotationprocessordemo/client/SimpleAnnotationsTest.java
--- Processors/src/main/java/com/qinxiandiqi/annotationprocessordemo/processors/ComplexityProcessor.java
+++ Processors/src/main/java/com/qinxiandiqi/annotationprocessordemo/processors/ComplexityProcessor.java
@@ -10,6 +10,7 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import java.io.*;
 import java.util.Set;
 
 /**
@@ -20,11 +21,38 @@ import java.util.Set;
 public class ComplexityProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        File file = new File("compile.log");
+        FileOutputStream os = null;
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            os = new FileOutputStream(file, true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         for (Element elem : roundEnv.getElementsAnnotatedWith(Complexity.class)) {
             Complexity complexity = elem.getAnnotation(Complexity.class);
-            String message = "annotation found in " + elem.getSimpleName()
+            String message = System.currentTimeMillis() + " - annotation found in " + elem.getSimpleName()
                     + " with complexity " + complexity.value();
+            try {
+                os.write(message.getBytes());
+                os.write("\r\n".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+        }
+
+        try {
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return true; // no further processing of this annotation type
     }
